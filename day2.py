de724ca12b3564fba837b4eb6589625b456e30f6
Cody-Macedo <macedo.cody@gmail.com>
2021-12-02 09:26:54 +0100

    add day2 update

--- day2.py
+++ day2.py
@@ -1,9 +1,19 @@
-measurements = open("day1.txt").readlines()
-# print(measurements)
+horizontal = 0;
+depth = 0;
 
-total = 0
-for i in range(len(measurements)):
-    if int(measurements[i]) > int(measurements[i - 1]):
-        total += 1
+instructions = open("day2.txt").readlines()
+# print(instructions)
+for i in range(len(instructions)):
+    splited = instructions[i].split(" ")
+    print(splited[0] + splited[1])
+    if splited[0] == "forward":
+        horizontal += int(splited[1])
+    elif splited[0] == "down":
+        depth += int(splited[1])
+    else:
+        depth -= int(splited[1])
 
-print(total)
+print(horizontal);
+print(depth);
+res = horizontal*depth
+print(res);
